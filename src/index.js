61b598349f9031e3efc097ae93d54ca40aad0a27
gladdaria <gladyscheva.dasha@yandex.ru>
2022-01-31 18:23:19 +0300

    feat: solve the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  let str = (Math.abs(n)).toString();
+
+  for(let i = 0; i < str.length; i++) {
+      result = `${str[i]}${result}`;
+  }
+  return result;
 }
